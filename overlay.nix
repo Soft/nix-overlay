b1df89046ec60b788d37ea5d9df3e6755a3c6a26
Samuel Laurén <samuel.lauren@iki.fi>
2019-02-09 23:57:54 +0200

    Add la-capitaine-icon-theme

--- overlay.nix
+++ overlay.nix
@@ -4,4 +4,5 @@ selft: super:
   rainbowpath = super.callPackage ./pkgs/rainbowpath {};
   run-or-raise = super.callPackage ./pkgs/run-or-raise {};
   cargo-watch = super.callPackage ./pkgs/cargo-watch {};
+  la-capitaine-icon-theme = super.callPackage ./pkgs/la-capitaine-icon-theme {};
 }
