ab07e45b3d563ddfce064702ab8efa61a5a48315
Samuel Laurén <samuel.lauren@iki.fi>
2019-08-07 19:17:27 +0300

    Update dnsproxy to 0.17.2

--- pkgs/dnsproxy/default.nix
+++ pkgs/dnsproxy/default.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   name = "dnsproxy-${version}";
-  version = "0.17.0";
+  version = "0.17.2";
 
   src = fetchFromGitHub {
     owner = "AdguardTeam";
     repo = "dnsproxy";
     rev = "v${version}";
-    sha256 = "0nk90nyvygdann5n741b4152c9h8dmzxd64lmha65cynw6lyc7nx";
+    sha256 = "11r88yxggp47fh8f37rakqr5w8amjg7f45arr8ybk41m9dllb5z0";
   };
 
-  modSha256 = "10icr98vz82ymhawnahcfqjm3mrs7a5x48h3av4qbqzghzz8pa2r";
+  modSha256 = "0ii9a1rbqvwfraji1ghw1408difxiwss3gsbbkljwp25qmpzcavc";
 
   meta = with stdenv.lib; {
     homepage = https://github.com/AdguardTeam/dnsproxy;
